a6020deb336de829d3cc5c125e7b6bf161d31c65
gnativyuriy <hnativ.yuriy@gmail.com>
2022-02-10 15:20:11 +0200

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { v4 as uuidv4 } from "uuid";
 import { faker } from "@faker-js/faker";
 import Slide from "@mui/material/Slide";
 import { Collapse } from "@mui/material";
+import "./items.css";
 
 import { makeStyles } from "@mui/styles";
-import { TransitionGroup } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -18,16 +19,32 @@ const useStyles = makeStyles(() => ({
     maxWidth: "1250px",
     minHeight: "100vh",
     backgroundColor: "rgb(216, 212, 212)",
+    transition: "0.2s easy",
   },
   noti: {
     position: "fixed",
     bottom: "20%",
     right: "15px",
+    height: "auto",
+    overflow: "visible",
+    transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    transitionProperty: "height,    transition-duration: 300ms",
+    transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+    transitionDelay: "0ms",
     // display: "flex",
     // flexDirection: "column",
     // width: "30%",
   },
+  itemContainer: {
+    height: "auto",
+    overflow: "visible",
+    transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    transitionProperty: "height,    transition-duration: 300ms",
+    transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+    transitionDelay: "0ms",
+  },
   alertclass: {
+    // transitionDuration: "500ms !important"
     transition: ".5s ease",
   },
 }));
@@ -55,25 +72,27 @@ function App() {
       >
         add
       </button>
-      <TransitionGroup>
-        <Stack className={classes.noti}>
-          {notiStack.map((i: IAlertItem, index: number) => {
-            return (
-              <Collapse key={index}>
-                <Slide in={true} direction="up" timeout={500}>
-                  <Alert
-                    className={classes.alertclass}
-                    icon={<CheckIcon fontSize="inherit" />}
-                    key={index}
-                    severity="success"
-                  >
-                    {i.text}
-                  </Alert>
-                </Slide>
-              </Collapse>
-            );
-          })}
-        </Stack>
+      <TransitionGroup className={classes.noti}>
+        {notiStack.map((i: IAlertItem, index: number) => {
+          return (
+            // <Collapse in={true}>
+            // </Collapse>
+            <div className={classes.itemContainer}>
+              <Slide in={true} direction="up" timeout={500}>
+                <Alert
+                  className={classes.alertclass}
+                  icon={<CheckIcon fontSize="inherit" />}
+                  key={index}
+                  severity="success"
+                >
+                  {i.text}
+                </Alert>
+              </Slide>
+            </div>
+            //             <CSSTransition key={index} timeout={500} classNames="item">
+            // </CSSTransition>
+          );
+        })}
       </TransitionGroup>
 
       {/* <Stack sx={{ width: "20%" }} spacing={2} className={classes.noti}> */}
